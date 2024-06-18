6f0870fc9cafc38d49656655b21506d42eb6fe76
GloriaSanabria <137516784+GloriaSanabria@users.noreply.github.com>
2024-06-18 15:56:37 -0400

    Add files via upload

---
+++ dia6.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario la temperatura en Celsius
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+
+# Convertir Celsius a Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Mostrar resultado
+print(f"La temperatura en grados Fahrenheit es: {fahrenheit}")
